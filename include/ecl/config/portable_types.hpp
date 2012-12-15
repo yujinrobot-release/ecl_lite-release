69f85f3180a16e458e72ee2e4fc73d2e241088f1
Daniel Stonier <d.stonier@gmail.com>
2012-12-15 20:11:05 +0900

    Rebase from 'release/ecl_config' @ version '0.50.2'

--- include/ecl/config/portable_types.hpp
+++ include/ecl/config/portable_types.hpp
@@ -71,6 +71,8 @@ namespace ecl {
 #endif
 #if ECL_SIZE_OF_DOUBLE == 8
 	typedef double float64; /**< @brief An alias for the platform's 64 bit float type. **/
+#elif ECL_SIZE_OF_LONG_DOUBLE == 8
+	typedef long double float64; /**< @brief An alias for the platform's 64 bit float type. **/
 #else
 	#error "ecl::float64 could not be typedef'd (probably faulty ecl logic)."
 #endif
@@ -79,7 +81,9 @@ namespace ecl {
 #elif ECL_SIZE_OF_LONG_DOUBLE == 16
 	typedef long double float128; /**< @brief An alias for the platform's 128 bit float type. **/
 #else
-	#error "ecl::float96 could not be typedef'd (probably faulty ecl logic)."
+	#define ECL_LONG_DOUBLE_UNDEFINED
+	#define ECL_LONG_LONG_DOUBLE_UNDEFINED
+	//#error "ecl::float96 could not be typedef'd (probably faulty ecl logic)."
 #endif
 
 } // namespace ecl
